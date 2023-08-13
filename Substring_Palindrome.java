183624309df85076f545ae32f88b6ce8d2b9cf58
Shruti Sharma <53565103+ShrutiSharma-27@users.noreply.github.com>
2023-08-13 18:03:10 +0530

    Create Substring_Palindrome.java

---
+++ Substring_Palindrome.java
@@ -0,0 +1,21 @@
+public class Substring_Palindrome {
+    public static void main(String[] args){
+        String s = "abccbc";
+        for(int i=0 ; i<s.length() ; i++){
+            for(int j=i+1 ; j<=s.length() ; j++){
+                String subSTR = s.substring(i,j);
+                if(isPalindrome(subSTR)) System.out.println(subSTR);
+            }
+        }
+    }
+
+    static boolean isPalindrome(String s){
+        int start = 0;
+        int end = s.length()-1;
+        while(start<=end){
+            if (s.charAt(start)!=s.charAt(end)) return false;
+            else start++; end--;
+        }
+        return true;
+    }
+}
